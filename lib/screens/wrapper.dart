0fc660acd063743af6228bb4355f6b0b6b8f81bf
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-10-04 17:10:02 +0800

    update

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_project/screens/home/home.dart';
+import 'package:flutter_firebase_project/screens/authenticate/sign_in.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
@@ -7,8 +7,8 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either home or authenticate widget
-    return Home(
-      key: key,
+    return Container(
+      child: SignIn(),
     );
   }
 }
