5bdc9e3229bd543624bc3d0d73c885facc25b029
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-10-11 21:28:53 +0800

    bottom sheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,6 +12,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: const Text('bottom sheet'),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>?>.value(
       value: DatabaseService(uid: '').brews,
       initialData: null,
@@ -32,6 +44,14 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.settings, color: Colors.black),
+              label: const Text(
+                'Settings',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: () => showSettingsPanel(),
             )
           ],
         ),
